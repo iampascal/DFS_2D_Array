92cf8c7bbf06b600382bc27bb9d868cc6f07a915
Pascal <126212787+iampascal@users.noreply.github.com>
2024-02-08 18:17:31 +1100

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
+//
+// Created by Pascal Terwesten on 8/2/2024.
+//
+
 #include <iostream>
+#include "header.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
+
+
+
+
+
 }
