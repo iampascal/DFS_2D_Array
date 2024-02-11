61bd8c47b2992280b5c10aaf1226a041fe1d1c77
Pascal <126212787+iampascal@users.noreply.github.com>
2024-02-11 20:58:52 +1100

    Version 1.1

--- main.cpp
+++ main.cpp
@@ -38,22 +38,51 @@ void updateConnectedElement(vector<vector<int>>& inputMatrix, int xPosition, int
     stack<pair<int,int>> neighbourStack;
     neighbourStack.push({xPosition, yPosition});
 
+    int order = 1;
+
+
     while (!neighbourStack.empty()) {
+
+
+
+
         pair<int, int> current = neighbourStack.top();
+
+        // Display the top element
+        pair<int, int> topElement = neighbourStack.top();
+        cout << "Top element of the stack: (" << topElement.first << ", " << topElement.second << ")" << endl;
+        printStack(neighbourStack);
+        // Pop the top element
         neighbourStack.pop();
+
 
         int row = current.first;
         int col = current.second;
 
         visited[row][col] = true;
-        cout << "(" << row << "," << col << ") ";
+        //cout << "(" << row << "," << col << ") ";
+
+
+        inputMatrix[row][col] = order;
+        order = order + 1;
 
         for (int i = 0; i < 4; ++i) {
             int newRow = row + xDirection[i];
             int newCol = col + yDirection[i];
 
             if (isValid(newRow, newCol, inputMatrix) && !visited[newRow][newCol]) {
+                // Push the new element onto the stack
                 neighbourStack.push({newRow, newCol});
+
+                // Access the newly pushed element directly
+                pair<int, int> pushedElement = {newRow, newCol};
+
+                // Print the pushed element
+                cout << "Pushed to stack element (" << pushedElement.first << ", " << pushedElement.second << ")" << endl;
+
+                // Mark the new element as visited
+                visited[newRow][newCol] = true;
+
             }
         }
     }
@@ -61,11 +90,11 @@ void updateConnectedElement(vector<vector<int>>& inputMatrix, int xPosition, int
 
 int main() {
     vector<vector<int>> matrix = {
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0},
     };
 
     updateConnectedElement(matrix, 0, 0);
